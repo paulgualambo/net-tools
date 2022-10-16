a0b6d70326b7391993f8c58a95e7933454fad127
Paul Gualambo <paul.gualambo@gmail.com>
2022-10-16 11:44:02 -0500

    feat/ConcetApiClient definition get api

---
+++ ConectApiClient/app/UseCase/ProductUC.cs
@@ -0,0 +1,39 @@
+﻿using ConnectApiClient.Entities;
+using ConnectApiClient.Services;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectApiClient.UseCase
+{
+    public class ProductUC
+    {
+        public ProductUC()
+        {
+        }
+
+        public async Task<List<Product>> List()
+        {
+            try
+            {
+                var service = new ProductoService();
+                var lista = await service.List();
+
+                foreach (var item in lista)
+                {
+                    item.Price = 10;
+                }
+
+                return lista;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
